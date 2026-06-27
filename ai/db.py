19eabec3272318b386d621e4aca848eac0b91130
AnoukDunu <52993706+AnoukDunu@users.noreply.github.com>
2026-06-27 14:42:26 +1000

    Added the db connection to the AI layer. Read comments for why it is redundant (I'm not 100% sure sold on the reason either)

---
+++ ai/db.py
@@ -0,0 +1,21 @@
+# Created this db connection (which might seem redundant since the base ETL pipeline already has a similar file)
+# Because ETL pipeline does batch inserts and bulk writes.
+# This AI layer only does small more infrequent reads so I wanted to keep the two seperate (even though they do almost the same thing)
+# This also uses the "psycopg2" import to execute queries directly into the database
+
+import os
+from dotenv import load_dotenv
+import psycopg2
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Database configuration
+def get_engine():
+    return psycopg2.connect(
+        host = os.getenv('DB_HOST'),
+        name = os.getenv('DB_NAME'),
+        user = os.getenv('DB_USER'),
+        password = os.getenv('DB_PASSWORD')
+    )
+    
---
+++ ai/query_engine.py
@@ -0,0 +1,1 @@
+# This file is what wil run SQL
