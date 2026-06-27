af62a4670e04922a175c8b8b8a328c8aac2e4ff4
AnoukDunu <52993706+AnoukDunu@users.noreply.github.com>
2026-06-27 15:13:02 +1000

    Added this file to talk to my LLM (or SLM)

---
+++ ai/llm.py
@@ -0,0 +1,3 @@
+# This file is what talks to the local LLM (or SLM because my little laptop isn't capable as the billion dollar datacenters polluting the world lol)
+import ollama
+# idk how to do this part
