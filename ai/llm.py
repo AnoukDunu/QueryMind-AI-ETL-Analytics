745002e25fc4032448b9026dfca2b6689253c6b9
AnoukDunu <52993706+AnoukDunu@users.noreply.github.com>
2026-07-01 00:28:21 +1000

    added extra comments

--- ai/llm.py
+++ ai/llm.py
@@ -1,4 +1,7 @@
 # This file is what talks to the local LLM (or SLM because my little laptop isn't capable as the billion dollar datacenters polluting the world lol)
-import ollama
-    # Install ollama in your environment if you want local LLM support.
+# Install ollama in your environment if you want local LLM support.
 # idk how to do this part
+
+# used the following command to install it. Kinda important and took me way too long to figure out:
+# python -m pip install ollama
+import ollama
