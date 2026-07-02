1001c394a58a0f96c0049741e4e61b83c6b60c95
AnoukDunu <52993706+AnoukDunu@users.noreply.github.com>
2026-07-02 12:17:12 +1000

    coded to instruct LLM to explain returned results from the db

---
+++ ai/response_generator.py
@@ -0,0 +1,16 @@
+# This file is to make the results more human-readable (in natural language basically)
+from llm import ask_llm
+
+def explain_results(question, data):
+    prompt = f"""
+    You are a data analyst. You will be given a question and the results of a SQL query. 
+    Your task is to explain the results in natural language, providing insights and context based on the data.
+
+    Question: {question}
+    Data: {data}
+
+    Only return the explanation. Do not include any SQL queries or additional text.
+    Explain the answer clearly.
+    """
+
+    return ask_llm(prompt)
